4c2081cb841f80e875f30730750b5551ecd210bb
JesManCar <jesmancar99@gmail.com>
2025-07-21 22:30:39 +0200

    fix: :bug: Fix cors

--- index.js
+++ index.js
@@ -18,16 +18,12 @@ const corsOptions = {
   credentials: true,
   optionSuccessStatus: 200
 }
-
-
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-    bodyParser.urlencoded({
-      extended: true,
-    })
-  );
+
 
 app.get('/', (req, res) => {
   res.send('Hello World desde Express');

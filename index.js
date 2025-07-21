6cf31b16fc5e416afcd62279a85b2b33f3680a72
JesManCar <jesmancar99@gmail.com>
2025-07-21 22:38:26 +0200

    fix: :bug: Fix Cors v2

--- index.js
+++ index.js
@@ -18,12 +18,17 @@ const corsOptions = {
   credentials: true,
   optionSuccessStatus: 200
 }
+
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options('*', cors(corsOptions)); // Pre-flight requests
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  );
 
 app.get('/', (req, res) => {
   res.send('Hello World desde Express');
